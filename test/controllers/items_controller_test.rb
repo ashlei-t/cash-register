a5609d0de93b94c08dedb07c45826cb3d330bd15
ashlei-t <a.temena@gmail.com>
2025-07-16 23:18:41 +0200

    fixed failing tests for controller test and fixtures

--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class ItemsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get items_index_url
+    get items_url
     assert_response :success
   end
 end
